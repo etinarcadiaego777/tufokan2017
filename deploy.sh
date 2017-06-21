b62e51bd0aa95a8f6f0e3938d22818a26867fd06
Horváth Csaba <cshorvath@localhost.localdomain>
2017-06-21 20:42:18 +0200

    travis fix again

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@
 set -e
 
 # deployment via ftp upload. Using FTPS for that
-lftp -c "set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
+sudo apt-get install -y lftp
+lftp -c "set ftps:initial-prot ''; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
