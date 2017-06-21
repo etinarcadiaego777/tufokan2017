74c7f6b784e0885ee7176618f58c2bc93068f0ce
Horváth Csaba <cshorvath@localhost.localdomain>
2017-06-21 21:46:48 +0200

    deploy.sh change

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 # deploy.sh
-set -e
-
-# deployment via ftp upload. Using FTPS for that
-sudo apt-get install -y lftp
 lftp -c "open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
