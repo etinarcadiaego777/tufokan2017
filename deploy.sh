bc84474827e587496e129247d10caf48e9fb6f42
Horváth Csaba <cshorvath@localhost.localdomain>
2017-06-23 01:20:53 +0200

    travis fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # deploy.sh
-lftp -c "$FTP_INIT; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
+lftp -c "$FTP_INIT; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRnv public .; quit;"
