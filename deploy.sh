e2aca09dedb8cc8d01398f037b394f9c62f6409c
Horváth Csaba <cshorvath@localhost.localdomain>
2017-06-22 21:12:22 +0200

    deploy.sh fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # deploy.sh
-lftp -c "set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
+lftp -c "$FTP_INIT; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
